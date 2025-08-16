fa1049d05b485784af963ca40a7c7e4079af7158
minigamerking <pocketmonsterlogin@gmail.com>
2025-08-15 21:54:39 -0400

    Added a "Feeling Lucky" button
    It just finds a random modrinth project and displays it where the "cards" should be

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const searchBar = document.getElementById("searchBar");
+const luckyButton = document.getElementById("feeling-lucky");
 
-searchBar.addEventListener("input", fetchData);
+searchBar.addEventListener("input", fetchSearchData);
+luckyButton.addEventListener("click", fetchRandomData);
 
-fetchData();
+fetchSearchData();
 
-async function fetchData() {
+async function fetchSearchData() {
   console.clear();
   try {
     const res = await fetch(
       "https://api.modrinth.com/v2/search?query=" +
         searchBar.value +
-        "&limit=5&index=relevance"
+        "&limit=20&index=relevance"
     );
     const data = await res.json();
     showData(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchRandomData() {
+  console.clear();
+  try {
+    const res = await fetch(
+      "https://api.modrinth.com/v2/projects_random?count=20"
+    );
+    const data = await res.json();
+    showRandomData(data);
   } catch (error) {
     console.error(error);
   }
@@ -40,4 +55,34 @@ function showData(data) {
     project_card.appendChild(link);
     cards.appendChild(project_card);
   }
+}
+
+function showRandomData(data) {
+  const cards = document.getElementById("cards");
+  while (cards.firstChild) {
+    cards.removeChild(cards.firstChild);
+  }
+
+  const random = randInt(0, data.length);
+
+  const mod = data[random];
+
+  console.log("Adding card");
+  const slug = mod["slug"];
+  const icon = mod["icon_url"];
+  const desc = mod["description"];
+  const title = mod["title"];
+  let project_card = document.createElement("div");
+  project_card.className = "project-card";
+  let link = document.createElement("a");
+  link.href = "https://modrinth.com/project/" + slug;
+  link.textContent = title;
+  link.className = "mod-link";
+  link.target = "_blank";
+  project_card.appendChild(link);
+  cards.appendChild(project_card);
+}
+
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
